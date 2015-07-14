776a60d3499f8dead169e298825a7d0431b24197
Mircea Alexandru <mircea.alexandru@gmail.com>
2015-07-14 16:21:41 +0300

    changes to match current seneca-auth implementation

--- local-auth.js
+++ local-auth.js
@@ -1,19 +1,28 @@
 var passport_local = require('passport-local')
-
 var LocalStrategy = passport_local.Strategy
 
-
-module.exports = function (conf, passport, done) {
+module.exports = function (options) {
   var seneca = this
+  var service = 'local'
 
-  passport.use(new LocalStrategy(
+  var authPlugin = new LocalStrategy(
     function (username, password, done) {
       seneca.act({role: 'user', cmd: 'login', nick: username, email: username, password: password},
         function (err, out) {
           done(!out.ok ? out : null, out)
         })
     }
-  ))
+  )
 
-  done()
+  var prepareLoginData = function (args, cb){
+    cb(null, args)
+  }
+
+  seneca.add({role: 'auth', cmd: 'prepareLocalLoginData'}, prepareLoginData)
+
+  seneca.act({role: 'auth', cmd: 'register_service', service: service, plugin: authPlugin, conf: options})
+
+  return {
+    name: 'local-auth'
+  }
 }
