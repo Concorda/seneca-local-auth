913218a835cb8f7a85c2c5a21b010e903009f2fa
mirceaalexandru <mircea.alexandru@gmail.com>
2015-07-15 14:01:15 +0300

    minimal changes

--- local-auth.js
+++ local-auth.js
@@ -9,16 +9,10 @@ module.exports = function (options) {
     function (username, password, done) {
       seneca.act({role: 'user', cmd: 'login', nick: username, email: username, password: password},
         function (err, out) {
-          done(!out.ok ? out : null, out)
+          done(out)
         })
     }
   )
-
-  var prepareLoginData = function (args, cb){
-    cb(null, args)
-  }
-
-  seneca.add({role: 'auth', cmd: 'prepareLocalLoginData'}, prepareLoginData)
 
   seneca.act({role: 'auth', cmd: 'register_service', service: service, plugin: authPlugin, conf: options})
 
