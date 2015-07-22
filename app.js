27d1524f508388f807af6739af08cc497e6c074b
mirceaalexandru <mircea.alexandru@gmail.com>
2015-07-22 14:21:33 +0300

    fix

---
+++ app.js
@@ -0,0 +1,29 @@
+var passport_local = require('passport-local')
+var LocalStrategy = passport_local.Strategy
+
+module.exports = function (options) {
+  var seneca = this
+  var service = 'local'
+
+  var authPlugin = new LocalStrategy(
+    function (username, password, done) {
+      seneca.act({role: 'user', cmd: 'login', nick: username, email: username, password: password},
+        function (err, out) {
+          if (err){
+            return done(err)
+          }
+          if (!out.ok){
+            return done(out.why)
+          }
+          done(err, out)
+        })
+    }
+  )
+
+  seneca.act({role: 'auth', cmd: 'register_service', service: service, plugin: authPlugin, conf: options})
+
+  return {
+    name: 'local-auth'
+  }
+}
+
